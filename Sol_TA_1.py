87f0dc3358585c7b58e1098d0d22018c810b2247
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-11-09 11:47:51 +0530

    Add files via upload

---
+++ Sol_TA_1.py
@@ -0,0 +1,14 @@
+l1 = [1,2,3,4,5,7,9]
+print("The original list is : \n", l1)
+
+l1.insert(5,6)
+print("\nThe list after inserting 6 at position 5 is : \n", l1)
+
+l1.remove(9)
+print("\nThe list after removing element 9 is : \n", l1)
+
+l1.append(8)
+print("\nThe list after appending element 8 is is : \n", l1)
+
+l1.pop()
+print("\nThe list after popping out the last element is : \n", l1)
